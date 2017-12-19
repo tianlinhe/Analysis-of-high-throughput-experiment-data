7218e795b4f48f6370d0479038a52e86d2dfa582
tianlinhe <tinaho_ok@hotmail.com>
2017-12-19 16:21:16 +0100

    Update f_oneway.py

--- f_oneway.py
+++ f_oneway.py
@@ -2,30 +2,36 @@ import pandas as pd
 from scipy import stats
 #import the csv file
 df = pd.read_csv('diffacto_weightedsum.csv')
-
-#to format it in a more comprehensible way, remove the first column and set the protein name as row name, the new csv named "my_df"
+#print (df)
+#to format it in a more comprehensible way, remove the first column
 df.drop(df.columns[[0]], axis=1, inplace=True)
-my_df= df.set_index('protein')
 
+print (df.index)
 F_distribution = []
 Pvalue = []
 Result = []
-for row_num in range (len(my_df.index)):
+count = 0
+for row_num in range (len(df.index)):
 
-    f, p = stats.f_oneway(my_df.iloc[row_num][0:4],my_df.iloc[row_num][4:8],my_df.iloc[row_num][8:12],my_df.iloc[row_num][12:16],my_df.iloc[row_num][16:])
+    f, p = stats.f_oneway(df.iloc[row_num][1:5],df.iloc[row_num][5:9],df.iloc[row_num][9:13],df.iloc[row_num][13:17],df.iloc[row_num][17:])
     F_distribution.append(f)
     Pvalue.append(p)
 #here we set the p-value threshold as 0.05
     if p < 0.05:
         Result.append('yes')
+        count += 1
     else:
         Result.append('no')
 
 #add F_distribution, Pvalue and results as columns at the end of the table
 
-my_df['F_distribution'] = F_distribution
-my_df['Pvalue'] = Pvalue
-my_df['Result'] = Result
-#print (my_df)
+df['F_distribution'] = F_distribution
+df['Pvalue'] = Pvalue
+df['Result'] = Result
+print (count)
+
 #export the results as a new csv file
-my_df.to_csv('pvalue_weightedsum.csv')
+df.to_csv('pvalue_weightedsum.csv')
+
+
+
