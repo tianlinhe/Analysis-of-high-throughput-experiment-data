06f3f5df45dc4ad7454d4694c89c23579b79b687
tianlinhe <tinaho_ok@hotmail.com>
2017-12-19 16:43:48 +0100

    Update qvalue.py

--- qvalue.py
+++ qvalue.py
@@ -3,43 +3,51 @@ from scipy import stats
 #import the csv file
 df = pd.read_csv('diffacto_weightedsum.csv')
 
-#to format it in a more comprehensible way, remove the first column and set the protein name as row name, the new csv named "my_df"
+#to format it in a more comprehensible way, remove the first column
 df.drop(df.columns[[0]], axis=1, inplace=True)
-my_df= df.set_index('protein')
 
 F_distribution = []
 Pvalue = []
 
-#for row_num in range (0,3):
-for row_num in range (len(my_df.index)):
+for row_num in range (len(df.index)):
 
-    f, p = stats.f_oneway(my_df.iloc[row_num][0:4],my_df.iloc[row_num][4:8],my_df.iloc[row_num][8:12],my_df.iloc[row_num][12:16],my_df.iloc[row_num][16:])
+    f, p = stats.f_oneway(df.iloc[row_num][1:5],df.iloc[row_num][5:9],df.iloc[row_num][9:13],df.iloc[row_num][13:17],df.iloc[row_num][17:])
     F_distribution.append(f)
     Pvalue.append(p)
 
 #add F_distribution, Pvalue and results as columns at the end of the table
+
+df['F_distribution'] = F_distribution
+df['Pvalue'] = Pvalue
+
 sorted_Pvalue = Pvalue.sort(reverse=True)
+print (sorted_Pvalue)
 
-my_df['F_distribution'] = F_distribution
-my_df['Pvalue'] = Pvalue
+#print (my_df)
+#export the results as a new csv file
+df.to_csv('pvalue_weightedsum.csv')
 
-#sorted my_df according to P-value, so that the highest P-value is displayed on the top of the list
-sorted_df=my_df.sort(['Pvalue'], ascending = False)
-#print (sorted_df)
+#sort df according to P-value, so that the highest P-value is displayed on the top of the list
+sorted_df=df.sort_values(['Pvalue'], ascending = False)
+
 #Formula of Qvalue refer to the excel file in computerlab1
 Result = []
 Qvalue = [1]
+
+count = 0
 for row_num in range (len(sorted_df)):
+    #print (sorted_df.iloc[row_num]['Pvalue'])
 #important, q-value is the minimum FDR, so it always compares it with the preceding term, her the last term in the Qvalue list.
-    q = min(len(sorted_df)*Pvalue[row_num]/(len(sorted_df)-row_num),Qvalue[-1])
+    q = min(len(sorted_df)*sorted_df.iloc[row_num]['Pvalue']/(len(sorted_df)-row_num),Qvalue[-1])
     if q < 0.05:
         Result.append('yes')
+        count += 1
     else:
         Result.append('no')
     Qvalue.append(q)
 #print (len(Result))
 #print (len(Qvalue))
-
+print (count)
 #to remove the first term (1) from the list of q-value
 Qvalue.remove(1)
 sorted_df['Qvalue'] = Qvalue
@@ -47,3 +55,4 @@ sorted_df['Result'] = Result
 #print (Qvalue)
 #export the results as a new csv file
 sorted_df.to_csv('qvalue_weightedsum.csv')
+
